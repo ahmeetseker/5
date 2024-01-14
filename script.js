615120cad19939d2853e812c52d6cddd0d033367
ahmet <ahmetseker1905@hotmail.com.tr>
2024-01-14 21:32:20 +0300

    Clock

---
+++ script.js
@@ -0,0 +1,28 @@
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const pmamEl = document.getElementById("pmam");
+
+function updateClock(){
+     let h = new Date().getHours();
+     let m = new Date().getMinutes();
+     let s = new Date().getSeconds();
+     let pmam = "AM"
+
+     if(h > 12){
+        h = h -12
+        pmam ="PM";
+     } 
+
+     h=h<10 ? "0" + h : h;
+
+     hourEl.innerText = h;
+     minuteEl.innerText= m;
+     secondEl.innerText = s;
+     pmamEl , (innerText = pmam);
+     setTimeout(()=>{
+        updateClock()
+     },1000)
+}
+
+updateClock();
